eadba8abb4af441199f5d19e9bb52118d0845896
sabilulbari <mdsabilulbari100@gmail.com>
2026-05-20 13:45:10 +0600

    project setup

---
+++ index.js
@@ -0,0 +1,56 @@
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const express = require("express");
+const app = express();
+const cors = require("cors");
+
+
+require("dotenv").config();
+app.use(cors());
+app.use(express.json())
+
+const port = process.env.PORT;
+
+const uri = process.env.MONGO_URI;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    await client.connect();
+
+    const db = client.db("pet-pulse")
+    const allPetCollection = db.collection("All-added-pet")
+
+    app.post("/addPet", async(req, res)=>{
+        const allPets = await req.body;
+        const result = await allPetCollection.insertOne(allPets)
+        res.json(result)
+    })
+
+    app.get("/addPet", async(req, res)=>{
+      const result = await allPetCollection.find().toArray()
+      res.send(result)
+    })
+
+    app.get('/', async(req, res)=>{
+        res.send("Server is ready")
+    })
+    app.get("/add-pet", async (req, res) => {
+      res.send("This api is working");
+    });
+    
+  } finally {
+   //
+  }
+}
+run().catch(console.dir);
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
